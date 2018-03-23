f7b669720f3236a71e0d5af6e3dd7ff3413d6d0e
kevin <kevin321vip@126.com>
2018-03-23 09:33:28 +0800

    发送PING接受PONG

---
+++ app/src/main/java/com/example/kevin/websocket_stomp/socket/MySocketClient.java
@@ -0,0 +1,10 @@
+package com.example.kevin.websocket_stomp.socket;
+
+import WebSocketClient;
+
+/**
+ * Created by kevin on 2018/3/23.
+ */
+
+public class MySocketClient extends WebSocketClient {
+}
---
+++ app/src/main/java/com/example/kevin/websocket_stomp/socket/RealWebSocket.java
@@ -0,0 +1,8 @@
+package com.example.kevin.websocket_stomp.socket;
+
+/**
+ * Created by kevin on 2018/3/23.
+ */
+
+public class RealWebSocket {
+}
